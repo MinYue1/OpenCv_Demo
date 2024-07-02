223c7bb9d3d049c432d10670bfa56177498d9796
minyue <17674650736@163.com>
2024-07-02 16:29:47 +0800

    增加模板匹配功能模块

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,14 +32,24 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.pictureBox1.Location = new System.Drawing.Point(12, 133);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(956, 410);
+            this.pictureBox1.Size = new System.Drawing.Size(571, 519);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -77,11 +87,96 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button4.Location = new System.Drawing.Point(375, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(101, 45);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "轮廓检测";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button5.Location = new System.Drawing.Point(589, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(101, 45);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "斑点检测";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button6.Location = new System.Drawing.Point(482, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(101, 45);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "边缘检测";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox2.Location = new System.Drawing.Point(589, 133);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(577, 519);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button7.Location = new System.Drawing.Point(161, 73);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(101, 45);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "缩放截取";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button8.Location = new System.Drawing.Point(375, 73);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(101, 45);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "模板匹配";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button9.Location = new System.Drawing.Point(12, 73);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(101, 45);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "添加模板图片";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(980, 555);
+            this.ClientSize = new System.Drawing.Size(1178, 654);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -89,6 +184,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -99,6 +195,13 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,12 @@
 ﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using OpenCvSharp.WpfExtensions;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +21,7 @@ namespace OpenCv_Demo
             InitializeComponent();
         }
         private string curtimgFile;
+        private string targetImg;
 
 
         private void button1_Click(object sender, EventArgs e)
@@ -60,23 +64,176 @@ namespace OpenCv_Demo
                 pictureBox1.Image = new Bitmap(this.curtimgFile);
             }
         }
-
+        /// <summary>
+        /// 旋转
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button3_Click(object sender, EventArgs e)
         {
 
             Mat img = new Mat(this.curtimgFile, ImreadModes.Grayscale);
             Point2f center = new Point2f(img.Cols / 2f, img.Rows / 2f);//获取图像的中心点，图像宽度，高度 / 2f
             //使用了Cv2.GetRotationMatrix2D()函数构建旋转矩阵，然后使用Cv2.WarpAffine()函数进行仿射变换。
-            Mat matrix = Cv2.GetRotationMatrix2D(center, 90, 0.6);//旋转矩阵，绕中心点顺时针90°，并缩小为原来的60
+            Mat matrix = Cv2.GetRotationMatrix2D(center, 60, 0.6);//构建旋转矩阵，绕中心点顺时针90°，并缩小为原来的60
             
-
-            Mat xi = new Mat();
-            Cv2.Rotate(img, xi, RotateFlags.Rotate90CounterClockwise);
-            Cv2.WarpAffine(img, img, matrix, img.Size());//
+            Cv2.WarpAffine(img, img, matrix, img.Size());//执行旋转、平移、缩放等操作
             Cv2.ImShow("img", img);
             Cv2.WaitKey(0);//等待用户按键
             Cv2.DestroyAllWindows();//关闭所有图片窗口
 
+        }
+        /// <summary>
+        /// 轮廓检测
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Mat m1 = Cv2.ImRead(this.curtimgFile);//尝试读取为灰度图
+            Mat image = Cv2.ImRead(this.curtimgFile, ImreadModes.Color);
+
+            //转换为灰度图
+            Mat gray = new Mat();
+            Cv2.CvtColor(m1, gray, ColorConversionCodes.BGR2GRAY);
+
+            //二值化
+            Mat m2 = new Mat();
+            Cv2.Threshold(gray, m2, 150, 255, ThresholdTypes.Binary); 
+
+            this.pictureBox1.Image = BitmapConverter.ToBitmap(m2);
+
+            //
+            OpenCvSharp.Point[][] contours;
+            HierarchyIndex[] hierarchy;
+
+            Cv2.FindContours(m2, out contours, out hierarchy, RetrievalModes.List, ContourApproximationModes.ApproxSimple);
+
+            Scalar color = new Scalar(0, 0, 255); // 轮廓颜色为绿色
+            int thickness = 2; // 轮廓线粗细为2
+
+            for (int i = 0; i < contours.Length; i++)
+            {
+                Cv2.DrawContours(image, contours, i, color, thickness); // 绘制轮廓
+            }
+
+
+            this.pictureBox2.Image = BitmapConverter.ToBitmap(image);
+
+            Cv2.Resize(image,image,new OpenCvSharp.Size(image.Width / 3, image.Height / 3));
+
+            Cv2.ImShow("image", image);
+            Cv2.WaitKey(0);
+
+            
+        }
+        /// <summary>
+        /// 边缘检测
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Mat m1 = Cv2.ImRead(this.curtimgFile, ImreadModes.Grayscale);//尝试加载为灰度图
+
+
+            //高斯模糊
+            Mat dst = new Mat();
+            Cv2.GaussianBlur(m1, dst, new OpenCvSharp.Size(7, 7), 2, 2);
+
+            this.pictureBox2.Image = BitmapConverter.ToBitmap(dst);
+            MessageBox.Show("");
+
+            //边缘检测
+            Mat edges = new Mat();
+            Cv2.Canny(m1, edges, 100, 200);
+
+
+
+            //显示原有图像和边缘图像
+            this.pictureBox2.Image = BitmapConverter.ToBitmap(edges);
+            //Cv2.ImShow("原图", m1);
+            //Cv2.ImShow("结果", edges);
+            //Cv2.WaitKey(0);
+
+            //释放内存
+            Cv2.DestroyAllWindows();
+            m1.Dispose();
+            edges.Dispose();
+        }
+
+        /// <summary>
+        /// 斑点检测
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        /// <summary>
+        /// 缩放
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+            Mat m1 = new Mat(this.curtimgFile);
+
+            Mat dst1 = new Mat();
+            Cv2.Resize(m1, dst1, new OpenCvSharp.Size(m1.Width / 3, m1.Height / 3), interpolation: InterpolationFlags.Area);
+
+
+            this.pictureBox1.Image = BitmapConverter.ToBitmap(dst1);
+
+
+        }
+
+        /// <summary>
+        /// 模板匹配
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.curtimgFile) || string.IsNullOrEmpty(this.targetImg)) return;
+
+            //加载图片
+            Mat temp  = new Mat(this.targetImg,ImreadModes.AnyColor); //模板图
+            Mat main = new Mat(this.curtimgFile,ImreadModes.AnyColor); //被匹配的图
+            Mat result = new Mat(this.curtimgFile,ImreadModes.AnyColor);//结果图
+
+
+            //缩小图片，减少计算量
+
+            //模板匹配
+            Cv2.MatchTemplate(main, temp, result, TemplateMatchModes.CCoeffNormed);
+
+
+        }
+
+        /// <summary>
+        /// 添加目标图片
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button9_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            //dialog.Multiselect = true;//该值确定是否可以选择多个文件
+            dialog.Title = "请选择文件";
+            dialog.Filter = "图像文件(*.jpg;*.jpeg;*.png;*.bmp;*.gif;*.tiff)|*.jpg;*.jpeg;*.png;*.bmp;*.gif;*.tiff";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.targetImg = dialog.FileName;
+                Mat mat = new Mat(this.targetImg);
+                using(new Window("目标图片", mat))
+                {
+                    Cv2.WaitKey();
+                }
+            }
         }
     }
 }
